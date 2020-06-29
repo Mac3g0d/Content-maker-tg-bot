c871e87eb01bb2ab68c06614fc381188714c28eb
Vytua <55776449+Mac3g0d@users.noreply.github.com>
2020-06-29 18:39:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-token = "813998317:AAHM5q9GbVFyjtQLshB3dkK3dOO30RYfjNY"
-vk_tokens = ["9eca3c459eca3c459eca3c458b9ea1b5f299eca9eca3c45c3ebcbfb18876220645c88cb",
-             'cbf389fbcbf389fbcbf389fb7bcb9e3a9dccbf3cbf389fb964ac52881a2001d05259659',
-             'c5209a8cc5209a8cc5209a8cf1c54d5a76cc520c5209a8c989d84a8282bfc4fd11cee95',
-             'fd930e77fd930e77fd930e7724fdfece8bffd93fd930e77a02e10201aee253a604649b2',
-             'cfaa4693cfaa4693cfaa4693bbcfc7866eccfaacfaa46939217580d6dc66fd2b54c949b',
-             '1f1a28221f1a28221f1a28224e1f77e8dd11f1a1f1a282242a73704386d588c2a032b36']
-my_vk_token = "9eca3c459eca3c459eca3c458b9ea1b5f299eca9eca3c45c3ebcbfb18876220645c88cb"
+token = ""
+vk_tokens = ["",
+             '',
+             '',
+             '',
+             '',
+             '']
+my_vk_token = ""
 ver = 5.101
 count = 3
 offset = 0
 
-client_id = "vtDxiVfPjy4QAR0pecWrw1kd3uFXT86n"
-client_secret = "ND0iWwvkR5fJcnOuVh76Qd9tpjUIFKgy"
+client_id = ""
+client_secret = ""
 reg_vk = r'(?P<url>https?://[^\s]+)'  # регулярка для обрезки вк
 reg_tg = r'###(.*)\n(.*)'  # регулярка для обрезки сообщения перед отправкой в канал
 reg_ali = r'https?://aliexpress\.ru/item/.*.html|https?://www.aliexpress\.com/item/.*.html' # регулярка для обрезки али
@@ -19,10 +19,10 @@ reg_ali_kot = [r'#(.*)', r'⚡(.*)⚡', r'\n\n']
 reg_ali_kupon = r'✈(.*)✈'
 chat_id = '@wektnwrjntwei234'
 time_post = [0, 0, 0, 0, 0, 0, 0]
-chat_id_to_post = '-1001276416110'
-chat_id_to_post2 = '-1001449121567'
-cloud_name='themaceg0d'
-api_key='673539419288945'
-api_secret='MMLUq99j2kTA_wofxVBul2qAsKk'
+chat_id_to_post = '-'
+chat_id_to_post2 = '-'
+cloud_name=''
+api_key=''
+api_secret=''
 
 DEFAULT_TAG = "python_sample_basic"
